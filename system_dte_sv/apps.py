8bbca514f7041af87d42038afaaa8fbc9cbc97c0
Mario Roberto <mrgomez.dev@gmail.com>
2025-02-25 22:13:31 -0600

    Creaccion de modulos base para el proyecto

---
+++ system_dte_sv/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SystemDteSvConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'system_dte_sv'
---
+++ system_sales/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SystemSalesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'system_sales'
