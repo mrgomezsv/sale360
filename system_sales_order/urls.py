1e1c534ddf45f41ea59ce97f8f0ce4b0b7d2ec0e
Mario Roberto <mrgomez.dev@gmail.com>
2025-03-16 16:04:45 -0600

    Ya llama pedidos html desde el dashboard pero como pantalla externa

--- system_sales_order/urls.py
+++ system_sales_order/urls.py
@@ -1,7 +1,6 @@
-# system_sales_order/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pedidos/', views.lista_presupuestos, name='lista_presupuestos'),
+    path('pedidos/', views.pedidos_view, name='pedidos'),
 ]
--- system_sales_order/views.py
+++ system_sales_order/views.py
@@ -1,13 +1,4 @@
-# system_sales_order/views.py
 from django.shortcuts import render
 
-def lista_presupuestos(request):
-    # Lógica para obtener los datos de los presupuestos
-    presupuestos = [
-        {'numero': 1, 'cliente': 'Cliente A', 'total': 1000},
-        {'numero': 2, 'cliente': 'Cliente B', 'total': 2000},
-    ]
-    context = {
-        'presupuestos': presupuestos,
-    }
-    return render(request, 'lista_presupuestos.html', context)
+def pedidos_view(request):
+    return render(request, 'pedidos.html')
